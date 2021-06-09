e8cb7ecc7c4207477aa8c6766928f19688092726
David Levy <lennox.davidlevy@gmail.com>
2021-06-09 16:04:19 -0400

    fixed css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import Deployments from './Deployments/Deployments'
-import LeadTimes from './LeadTimes/LeadTimes'
-import RecoveryTimes from './RecoveryTimes/RecoveryTimes'
-import './App.css'
+import Deployments from './Deployments/Deployments';
+import LeadTimes from './LeadTimes/LeadTimes';
+import RecoveryTimes from './RecoveryTimes/RecoveryTimes';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="componentLayout">
-        <Deployments />
-        <RecoveryTimes />
+      <div className="app_title">Software Delivery Performance Metrics</div>
+      <div className="app_container">
+        <div className="componentLayout">
+          <Deployments />
+          <LeadTimes />
+        </div>
+        <div className="componentLayout">
+          <RecoveryTimes />
+        </div>
       </div>
-      <LeadTimes />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
