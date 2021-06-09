d79732c5e5a7555bcbe30dce46e78c0beb6580e5
David Levy <lennox.davidlevy@gmail.com>
2021-06-09 10:17:25 -0400

    style sheet added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Deployments from './Deployments/Deployments'
-
+import Deployments from './Deployments/Deployments';
+import './App.css';
 function App() {
   return (
     <div className="App">
       <Deployments />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Deployments/Deployments.js
+++ src/Deployments/Deployments.js
@@ -1,50 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import { format } from 'date-fns'
+import React, { useEffect, useState } from 'react';
+import { format } from 'date-fns';
 
 function Deployments() {
-  const [date, setDate] = useState('')
-  const [time, setTime] = useState('')
-  const [list, setList] = useState([])
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [list, setList] = useState([]);
   const handleDateChange = (e) => {
-    const { value } = e.target
-    setDate(value)
-  }
+    const { value } = e.target;
+    setDate(value);
+  };
   const handleTimeChange = (e) => {
-    const { value } = e.target
-    setTime(value)
-  }
+    const { value } = e.target;
+    setTime(value);
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    if (time.length === 0 || date.length === 0) return
-    const newDeployment = format(new Date(`${date} ${time}`), 'M/d/y h:mm:ss a')
+    e.preventDefault();
+    if (time.length === 0 || date.length === 0) return;
+    const newDeployment = format(
+      new Date(`${date} ${time}`),
+      'M/d/y h:mm:ss a'
+    );
     setList((prevList) => {
-      return [...prevList, newDeployment]
-    })
-    setTime('')
-    setDate('')
-  }
+      return [...prevList, newDeployment];
+    });
+    setTime('');
+    setDate('');
+  };
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        width: '400px',
-        backgroundColor: '#f3f3f3',
-        padding: '10px',
-      }}
-      className="deployment-container"
-    >
-      <div>Deployments</div>
+    <div className="container">
+      <div className="title">Deployments</div>
       <ol>
         {list.map((item, index) => {
-          return <li key={index}>{item}</li>
+          return <li key={index}>{item}</li>;
         })}
       </ol>
-      <label style={{ display: 'block' }} htmlFor="deployment-date">
-        Deployment Date
-      </label>
+      <label htmlFor="deployment-date">Deployment Date</label>
       <input
         id="deployment-date"
         name="deployment-date"
@@ -52,9 +44,7 @@ function Deployments() {
         value={date}
         onChange={handleDateChange}
       ></input>
-      <label style={{ display: 'block' }} htmlFor="deployment-time">
-        Deployment Time
-      </label>
+      <label htmlFor="deployment-time">Deployment Time</label>
       <input
         id="deployment-time"
         type="time"
@@ -62,14 +52,9 @@ function Deployments() {
         value={time}
         onChange={handleTimeChange}
       ></input>
-      <button
-        style={{ backgroundColor: '#5487e0', width: '150px', marginTop: '5px', color: 'white' }}
-        onClick={handleSubmit}
-      >
-        Add Deployment
-      </button>
+      <button onClick={handleSubmit}>Add Deployment</button>
     </div>
-  )
+  );
 }
 
-export default Deployments
+export default Deployments;
