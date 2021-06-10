7d3b2a2a0521f0c4f1366969c5370a90fd8542db
Josh Purcell <josh.purcell@ibm.com>
2021-06-10 12:42:10 -0400

    add localStorage to deployments

--- src/Deployments/Deployments.js
+++ src/Deployments/Deployments.js
@@ -1,62 +1,67 @@
-import React, { useEffect, useState } from 'react';
-import { format, getWeek } from 'date-fns';
+import React, { useEffect, useState } from 'react'
+import { format, getWeek } from 'date-fns'
 
 function Deployments() {
-  const [date, setDate] = useState('');
-  const [time, setTime] = useState('');
-  const [list, setList] = useState([]);
-  const [weekFrequency, setWeekFrequency] = useState({});
-  const [renderWeekFrequency, setRenderWeekFrequency] = useState('');
+  const [date, setDate] = useState('')
+  const [time, setTime] = useState('')
+  const [list, setList] = useState(() => {
+    return localStorage.getItem('deploymentsList')
+      ? JSON.parse(localStorage.getItem('deploymentsList'))
+      : []
+  })
+  const [weekFrequency, setWeekFrequency] = useState({})
+  const [renderWeekFrequency, setRenderWeekFrequency] = useState('')
 
   useEffect(() => {
-    const divisor = Object.keys(weekFrequency).length;
-    if (divisor < 1) return;
-    let sum = 0;
+    const divisor = Object.keys(weekFrequency).length
+    if (divisor < 1) return
+    let sum = 0
     for (let key in weekFrequency) {
-      sum += weekFrequency[key];
+      sum += weekFrequency[key]
     }
-    let average = sum / divisor;
-    average = average.toFixed(1);
-    let tempRenderWeekFrequency;
+    let average = sum / divisor
+    average = average.toFixed(1)
+    let tempRenderWeekFrequency
     if (average % 1 !== 0) {
-      tempRenderWeekFrequency = ` ${average}/week`;
+      tempRenderWeekFrequency = ` ${average}/week`
     } else {
-      average = Math.round(average);
-      tempRenderWeekFrequency = ` ${average}/week`;
+      average = Math.round(average)
+      tempRenderWeekFrequency = ` ${average}/week`
     }
-    setRenderWeekFrequency(tempRenderWeekFrequency);
-  }, [list]);
+    setRenderWeekFrequency(tempRenderWeekFrequency)
+  }, [list])
+
+  useEffect(() => {
+    localStorage.setItem('deploymentsList', JSON.stringify(list))
+  }, [list])
 
   const handleDateChange = (e) => {
-    const { value } = e.target;
-    setDate(value);
-  };
+    const { value } = e.target
+    setDate(value)
+  }
   const handleTimeChange = (e) => {
-    const { value } = e.target;
-    setTime(value);
-  };
+    const { value } = e.target
+    setTime(value)
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    if (time.length === 0 || date.length === 0) return;
-    const week = getWeek(new Date(date));
-    let tempWeekFrequency = weekFrequency;
+    e.preventDefault()
+    if (time.length === 0 || date.length === 0) return
+    const week = getWeek(new Date(date))
+    let tempWeekFrequency = weekFrequency
     if (tempWeekFrequency[week]) {
-      tempWeekFrequency[week] += 1;
+      tempWeekFrequency[week] += 1
     } else {
-      tempWeekFrequency[week] = 1;
+      tempWeekFrequency[week] = 1
     }
-    const newDeployment = format(
-      new Date(`${date} ${time}`),
-      'M/d/y h:mm:ss a'
-    );
+    const newDeployment = format(new Date(`${date} ${time}`), 'M/d/y h:mm:ss a')
     setList((prevList) => {
-      return [...prevList, newDeployment];
-    });
-    setWeekFrequency(tempWeekFrequency);
-    setTime('');
-    setDate('');
-  };
+      return [...prevList, newDeployment]
+    })
+    setWeekFrequency(tempWeekFrequency)
+    setTime('')
+    setDate('')
+  }
 
   return (
     <div className="container">
@@ -67,7 +72,7 @@ function Deployments() {
       </div>
       <ol>
         {list.map((item, index) => {
-          return <li key={index}>{item}</li>;
+          return <li key={index}>{item}</li>
         })}
       </ol>
       <label htmlFor="deployment-date">Deployment Date</label>
@@ -88,7 +93,7 @@ function Deployments() {
       ></input>
       <button onClick={handleSubmit}>Add Deployment</button>
     </div>
-  );
+  )
 }
 
-export default Deployments;
+export default Deployments
